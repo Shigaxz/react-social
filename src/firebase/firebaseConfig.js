68d2bb93a945e871709cb51f03f31161dc6a9e38
Valentin Cabrera <137360947+Shigaxz@users.noreply.github.com>
2025-03-05 15:05:42 -0300

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,14 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDO5yO5VcktZdjyg38vuqNpoOS4i86-tE4",
-  authDomain: "integracion-d00bf.firebaseapp.com",
-  databaseURL: "https://integracion-d00bf-default-rtdb.firebaseio.com",
-  projectId: "integracion-d00bf",
-  storageBucket: "integracion-d00bf.appspot.com",
-  messagingSenderId: "840054051270",
-  appId: "1:840054051270:web:60004893b8346d0b37e2c7",
-  measurementId: "G-3NG3Q1T6CM"
+  apiKey: "",
+  authDomain: "i",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
